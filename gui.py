051582cf04b25b1aa126dd8e5bcf084bd50464ae
Martin Manchev <72170828+mmanchev23@users.noreply.github.com>
2023-04-07 11:56:39 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -2,15 +2,12 @@
 
 import tkinter as tk
 
-# Create the calculator window
 root = tk.Tk()
-root.title("Simple Calculator")
+root.title("Python 3 GUI Calculator")
 
-# Create an entry widget to display the results
 entry = tk.Entry(root, width=35, borderwidth=5)
-entry.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
-# Define functions for button clicks and operations
 def button_click(number):
     current = entry.get()
     entry.delete(0, tk.END)
@@ -75,35 +72,29 @@ button_7 = tk.Button(root, text="7", padx=40, pady=20, command=lambda: button_cl
 button_8 = tk.Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
 button_9 = tk.Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
 button_0 = tk.Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
-button_add = tk.Button(root, text="+", padx=39, pady=20, command=button_add)
-button_subtract = tk.Button(root, text="-", padx=41, pady=20, command=button_subtract)
-button_multiply = tk.Button(root, text="*", padx=41, pady=20, command=button_multiply)
-button_divide = tk.Button(root, text="/", padx=41, pady=20, command=button_divide)
-button_equal = tk.Button(root, text="=", padx=91, pady=20, command=button_equal)
-button_clear = tk.Button(root, text="Clear", padx=79, pady=20, command=button_clear)
+button_add = tk.Button(root, text="+", padx=40, pady=20, command=button_add)
+button_subtract = tk.Button(root, text="-", padx=40, pady=20, command=button_subtract)
+button_multiply = tk.Button(root, text="*", padx=40, pady=20, command=button_multiply)
+button_divide = tk.Button(root, text="/", padx=40, pady=20, command=button_divide)
+button_equal = tk.Button(root, text="=", padx=40, pady=20, command=button_equal)
+button_clear = tk.Button(root, text="C", padx=40, pady=20, command=button_clear)
 
-# Add the buttons to the calculator window
 button_1.grid(row=3, column=0)
 button_2.grid(row=3, column=1)
 button_3.grid(row=3, column=2)
-
-button_4.grid(row=2, column=0)
-button_5.grid(row=2, column=1)
-button_6.grid(row=2, column=2)
-
-button_7.grid(row=1, column=0)
-button_8.grid(row=1, column=1)
-button_9.grid(row=1, column=2)
-
-button_0.grid(row=4, column=0)
-
-button_clear.grid(row=4, column=1, columnspan=2)
-button_add.grid(row=5, column=0)
-button_subtract.grid(row=6, column=0)
-button_multiply.grid(row=6, column=1)
-button_divide.grid(row=6, column=2)
-
-button_equal.grid(row=5, column=1, columnspan=2)
+button_add.grid(row=3, column=3)
+button_4.grid(row=4, column=0)
+button_5.grid(row=4, column=1)
+button_6.grid(row=4, column=2)
+button_subtract.grid(row=4, column=3)
+button_7.grid(row=5, column=0)
+button_8.grid(row=5, column=1)
+button_9.grid(row=5, column=2)
+button_multiply.grid(row=5, column=3)
+button_clear.grid(row=6, column=0)
+button_0.grid(row=6, column=1)
+button_equal.grid(row=6, column=2)
+button_divide.grid(row=6, column=3)
 
 def main() -> None:
     root.mainloop()
